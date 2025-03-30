938d9a50145b2a95e7b43a4be5351a1e2572c52d
Misha Kompaniec <mikokompaniec@gmail.com>
2025-03-30 23:42:48 +0300

    fixed scroll func

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import Drawer from '../drawer';
 
@@ -12,25 +13,27 @@ import {
 } from './style';
 
 const Header = () => {
-  const scrollToAbout = () => {
-    const aboutSection = document.getElementById('about-us');
-    if (aboutSection) {
-      aboutSection.scrollIntoView({ behavior: 'smooth' });
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const scrollToSection = (sectionId: string) => {
+    if (location.pathname !== '/') {
+      navigate('/', { replace: false });
+      setTimeout(() => {
+        scrollToElement(sectionId);
+      }, 100);
+    } else {
+      scrollToElement(sectionId);
     }
   };
 
-  const scrollToBestSellers = () => {
-    const aboutSection = document.getElementById('bestSellers');
-    if (aboutSection) {
-      aboutSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
-  const scrollToContacts = () => {
-    const contactsSection = document.getElementById('contacts');
-    if (contactsSection) {
-      contactsSection.scrollIntoView({ behavior: 'smooth' });
-    }
+  const scrollToElement = (sectionId: string) => {
+    setTimeout(() => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 50);
   };
 
   return (
@@ -40,9 +43,13 @@ const Header = () => {
       </LogoWrapper>
       <Menu>
         <MenuItemLink to="/store">МАГАЗИН</MenuItemLink>
-        <MenuItem onClick={scrollToAbout}>О НАС</MenuItem>
-        <MenuItem onClick={scrollToBestSellers}>ТОП ПРОДАЖ</MenuItem>
-        <MenuItem onClick={scrollToContacts}>КОНТАКТЫ</MenuItem>
+        <MenuItem onClick={() => scrollToSection('about-us')}>О НАС</MenuItem>
+        <MenuItem onClick={() => scrollToSection('bestSellers')}>
+          ТОП ПРОДАЖ
+        </MenuItem>
+        <MenuItem onClick={() => scrollToSection('contacts')}>
+          КОНТАКТЫ
+        </MenuItem>
         <Drawer />
       </Menu>
     </HeaderWrapper>
