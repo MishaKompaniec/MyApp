933c95ba5754af32aa9838e40003bca2f1764c03
Misha Kompaniec <mikokompaniec@gmail.com>
2025-06-11 21:35:10 +0300

    change contacts styles

--- src/pages/home/contacts/style.ts
+++ src/pages/home/contacts/style.ts
@@ -41,9 +41,8 @@ export const ContactList = styled.ul`
 
   li {
     width: fit-content;
-    background: ${colors.white};
     padding: 10px;
-    margin: 0 auto;
+    margin: 10px auto;
     border-radius: 5px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
   }
