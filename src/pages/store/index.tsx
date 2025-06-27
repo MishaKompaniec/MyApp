7d098579bab53a9a73fc9ce8c9db52c587337bcb
Misha Kompaniec <mikokompaniec@gmail.com>
2025-06-27 13:32:53 +0200

    add store translate

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,35 +1,40 @@
 import { Tabs } from 'antd';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import Bouquets from './bouquets';
 import EdibleBouquets from './edibleBouquets';
 import Plants from './plants';
 import { Wrapper } from './style';
 
-const Store = () => (
-  <Wrapper>
-    <Tabs
-      defaultActiveKey="bouquets"
-      tabPosition="left"
-      items={[
-        {
-          label: 'БУКЕТЫ',
-          key: 'bouquets',
-          children: <Bouquets />,
-        },
-        {
-          label: 'РАСТЕНИЯ',
-          key: 'plants',
-          children: <Plants />,
-        },
-        {
-          label: 'ФРУТЕТЫ',
-          key: 'fruit',
-          children: <EdibleBouquets />,
-        },
-      ]}
-    />
-  </Wrapper>
-);
+const Store = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Wrapper>
+      <Tabs
+        defaultActiveKey="bouquets"
+        tabPosition="left"
+        items={[
+          {
+            label: t('store.bouquets'),
+            key: 'bouquets',
+            children: <Bouquets />,
+          },
+          {
+            label: t('store.plants'),
+            key: 'plants',
+            children: <Plants />,
+          },
+          {
+            label: t('store.fruitBouquets'),
+            key: 'fruit',
+            children: <EdibleBouquets />,
+          },
+        ]}
+      />
+    </Wrapper>
+  );
+};
 
 export default Store;
